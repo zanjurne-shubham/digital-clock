5b1d3ffdba25d624f07a987e50f7134430dcf535
zanjurne-shubham <zanjurneshubham2005@gmail.com>
2024-12-29 13:17:43 +0530

    changeonjs

--- script.js
+++ script.js
@@ -13,24 +13,3 @@ minute.innerText=mtime;
 second.innerText=stime;
 },1000);
 
-
-
-
-// hour.innerText="10";
-// minute.innerText="11";
-// second.innerText="12";
-
-// // setInterval(()=>{
-
- 
-
-//     d=new Date();
-//     htime=d.getHours();
-//     mtime=d.getMinutes();
-//     stime=d.getSeconds();
-    
-//     hour.innerText=htime;
-//     minute.innerText=mtime;
-//     second.innerText=stime;
-
-//     },1000);
